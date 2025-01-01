c4cfa1cc9ac53bf7fb8dd89cd7ea0171794c2bea
abdullah-aladham <abdullah.aladham@gmail.com>
2025-01-01 22:51:43 +0200

    fixed some bugs and added some pages

--- frontend-ar/js/index.js
+++ frontend-ar/js/index.js
@@ -1,16 +1,21 @@
-var exampleModal = document.getElementById('exampleModal')
-exampleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  var recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  var modalTitle = exampleModal.querySelector('.modal-title')
-  var modalBodyInput = exampleModal.querySelector('.modal-body input')
+const html= document.getElementById("htmlPage");
+const body=document.getElementById("body");
+const navbar=document.getElementById("navbar")
+const checkbox = document.getElementById("checkbox");
+const chatContainer=document.getElementById("chat-container")
 
-  modalTitle.textContent = 'New message to ' + recipient
-  modalBodyInput.value = recipient
+checkbox.addEventListener("change",()=>{
+    if(checkbox.checked){
+        html.setAttribute('data-bs-theme',"dark");
+        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-dark");
+    body.setAttribute("color","#e0e1dd");
+    chatContainer.setAttribute("color","d590af")
+
+        
+    }else {
+        html.setAttribute("data-bs-theme","light");
+        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-light");
+        body.setAttribute("color","dark");
+
+    }
 })
--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
-//a code to open modal
-let exampleModal = document.getElementById('chat-with-us')
-exampleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  let button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  let recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  let modalTitle = exampleModal.querySelector('.modal-title')
-  let modalBodyInput = exampleModal.querySelector('.modal-body input')
+const html= document.getElementById("htmlPage");
+const body=document.getElementById("body");
+const navbar=document.getElementById("navbar")
+const checkbox = document.getElementById("checkbox");
+const wishlist=document.getElementById("wishlist");
+const cart =document.getElementById("cart");//#a47fa6
+checkbox.addEventListener("change",()=>{
+    if(checkbox.checked){
+        html.setAttribute('data-bs-theme',"dark");
+        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-dark");
+    body.setAttribute("color","#e0e1dd");
+    wishlist.setAttribute("filter"," invert(100%) sepia(1%) saturate(2253%) hue-rotate(16deg) brightness(93%) contrast(90%);")
 
-  modalTitle.textContent = 'New message to ' + recipient
-  modalBodyInput.value = recipient
+        
+    }else {
+        html.setAttribute("data-bs-theme","light");
+        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-light");
+        body.setAttribute("color","dark");
+
+    }
 })
