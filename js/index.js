8c6f94f4e3f63f82b677d5852a8ffaf5c99ee259
abdullah-aladham <abdullah.aladham@gmail.com>
2025-03-18 06:16:19 +0200

    fixed some path bugs

--- js/index.js
+++ js/index.js
@@ -10,16 +10,16 @@ checkbox.addEventListener("change",()=>{
         html.setAttribute('data-bs-theme',"dark");
         navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-dark");
     body.setAttribute("color","#e0e1dd");
-    wishlist.src="E:\\github\\folla-accessories\\images\\wishlist-darkmode.svg"
-    aboutusImg.src="E:\\github\\folla-accessories\\images\\folla5.png"
-        cart.src="E:\\github\\folla-accessories\\images\\cart-darkmode.svg"
+    wishlist.src="./images/wishlist-darkmode.svg"
+    aboutusImg.src="./images/folla5.png"
+        cart.src="./images/cart-darkmode.svg"
     }else {
         html.setAttribute("data-bs-theme","light");
         navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-light");
         body.setAttribute("color","dark");
-        wishlist.src="E:\\github\\folla-accessories\\images\\wishlist.svg"
-        aboutusImg.src="E:\\github\\folla-accessories\\images\\folla4.png"
-        cart.src="E:\\github\\folla-accessories\\images\\cart.svg"
+        wishlist.src="./images/wishlist.svg"
+        aboutusImg.src="./images/folla4.png"
+        cart.src="./images/cart.svg"
 
 
     }
